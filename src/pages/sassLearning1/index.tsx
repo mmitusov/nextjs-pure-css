7809539483d5d5f808e12a912d80ed8818ddd912
mmitusov <maksym.mitusov@gmail.com>
2023-05-25 15:14:01 +0300

    SASS Burger menu

--- src/pages/sassLearning1/index.tsx
+++ src/pages/sassLearning1/index.tsx
@@ -1,9 +1,20 @@
 import sass1 from '@/styles/sassLearning1.module.scss'
+import { useState } from 'react';
 
 const SaasLearninig1 = () => {
+    const [isBurgerOpen, setIsBurgerOpen] = useState(false)
+
     return (
         <div className={`${sass1.container}`}>
-            Hello
+            <p>Hello</p>
+            <button 
+                className={`${sass1.burgerBtn} ${isBurgerOpen ? sass1.burgerOpen : ''}`}
+                onClick={() => setIsBurgerOpen(!isBurgerOpen)}
+            >
+                <span className={`${isBurgerOpen ? sass1.span1 : ''}`}/>
+                <span className={`${isBurgerOpen ? sass1.span2 : ''}`}/>
+                <span className={`${isBurgerOpen ? sass1.span3 : ''}`}/>
+            </button>
         </div>
     );
 }
