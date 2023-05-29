1dd47346a0f6d5ad2a0295587baea8ca96b0a475
mmitusov <maksym.mitusov@gmail.com>
2023-05-29 19:08:06 +0300

    customHooks

---
+++ src/customHooks/useWindowDimentions.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+const useWindowDimentions = () => {
+    const [dimentions, setDimentions] = useState({
+        width: null,
+        height: null
+    })
+
+    useEffect(() => {
+        const updateSize = () => {
+            setDimentions({
+                width: window.innerWidth,
+                height: window.innerHeight
+            })
+        }
+        updateSize()
+ 
+        window.addEventListener('resize', updateSize);
+        return () => window.removeEventListener('resize', updateSize); console.log('resize');
+    }, [])
+
+    return {dimentions}
+}
+ 
+export default useWindowDimentions;
--- src/pages/burgerMenu1/index.tsx
+++ src/pages/burgerMenu1/index.tsx
@@ -1,12 +1,15 @@
+import useWindowDimentions from '@/customHooks/useWindowDimentions';
 import sass1 from '@/styles/burgerMenu1.module.scss'
 import { useState } from 'react';
 
 //Hardcoded Humburger solution
 const SaasLearninig1 = () => {
     const [isBurgerOpen, setIsBurgerOpen] = useState(false)
+    const {dimentions} = useWindowDimentions()
 
     return (
         <div className={`${sass1.container}`}>
+            <div>Width {dimentions.width}</div>
             <button 
                 className={`${sass1.burgerBtn} ${isBurgerOpen ? sass1.burgerOpen : ''}`}
                 onClick={() => setIsBurgerOpen(!isBurgerOpen)}
