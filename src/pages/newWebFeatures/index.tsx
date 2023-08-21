5ce06c7eebed04a9b6164a63c093ca5c96277d0e
mmitusov <maksym.mitusov@gmail.com>
2023-08-21 18:16:13 +0300

    Clean-up function

--- src/pages/newWebFeatures/index.tsx
+++ src/pages/newWebFeatures/index.tsx
@@ -3,7 +3,36 @@ import { useRef } from "react";
 //В HTML появился новый тег - <dialog />
 const newWebFeatures = () => {
     const dialogId:any = useRef()
+    //Standart way
+    useEffect(() => {
+        const unsubcsribe = onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {
+            setPosts(snapshot.docs)
+        })
+
+        return () => {
+            unsubcsribe();
+        }
+    }, [db])
     
+    //More clean 'return'
+    useEffect(() => {
+        const unsubcsribe = onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {
+            setPosts(snapshot.docs)
+        })
+
+        return unsubcsribe();
+    }, [db])
+
+    //we can attach 'return' directly
+    useEffect(() => {
+        return onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {setPosts(snapshot.docs)})
+    }, [db])
+
+    //With inplicit return
+    useEffect(() => 
+        onSnapshot(query(collection(db, 'posts'), orderBy('timestamp', 'desc')), snapshot => {setPosts(snapshot.docs)}
+    ), [db])
+
     return (
         <div>
             <dialog ref={dialogId}>
